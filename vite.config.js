8eb574449c1a73a9f27182a9b8e75782c227e720
vanolucas <10572472+vanolucas@users.noreply.github.com>
2026-03-25 23:45:53 +0100

    fix: Vite base (#2)
    To work with custom subdomain.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
-  base: "/who-will-win-the-world-cup/",
+  base: "/",
   publicDir: "../data",
   build: {
     outDir: "../dist",
